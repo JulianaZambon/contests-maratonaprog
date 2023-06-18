ed11e465335db1fd13b3fc1564a1156a2e830ad4
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2023-06-18 16:50:48 -0300

    Update a.cpp

--- fase_zero/a.cpp
+++ fase_zero/a.cpp
@@ -4,6 +4,14 @@ using namespace std;
 using ll = long long;
 
 int main() {
+    string partida;
+    cin >> partida;
+    if (partida == "XXO" || partida == "OXX" || partida == "XXO") {
+        cout << "Alice\n";
+    } else if (partida == "XOX") {
+        cout << "*\n";
+    } else
+        cout << "?\n";
 
     return 0;
 }
